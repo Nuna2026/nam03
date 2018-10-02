2b60911e2383e6149493c04f313263c3901c749a
nam1509 <mylovenam126@gmail.com>
2018-10-02 14:33:38 +0700

    new

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CharacterCard from './CharacterCard';
+const word = "Hello";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="card">
+        {
+          Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+        }
+     </div>
+    );
+  }
+}
+
+export default App;
